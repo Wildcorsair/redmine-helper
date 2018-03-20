7a78ba0102eef39dc97058747ee7e90faa1a1d77
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-03-20 23:04:50 +0200

    Added security rules, login functionality and dashboard page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,18 +6,38 @@ use AppBundle\Form\LoginType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
+        $message = '';
+
         $login = $this->createForm(LoginType::class);
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        if (isset($error) && !empty($error)) {
+            $message = $error->getMessage();
+        }
+
+        $lastUserName = $authenticationUtils->getLastUsername();
 
         return $this->render('default/index.html.twig', [
-            'login' => $login->createView()
+            'login' => $login->createView(),
+            '_username' => $lastUserName,
+            'error'     => $message
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
     }
 }
