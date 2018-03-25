b34962db5353f05f2a6e1e4a6b89d72637fb3e6a
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-03-25 09:35:53 +0300

    Added ProjectController and project functionality moved to it from DashboardController.

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -2,21 +2,11 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Form\ProjectType;
-use AppBundle\Service\RedmineRequestService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends Controller
 {
-    private $redmineRequestService;
-
-    public function __construct(RedmineRequestService $redmineRequestService)
-    {
-        $this->redmineRequestService = $redmineRequestService;
-    }
-
     /**
      * @Route("/dashboard", name="dashboard")
      *
@@ -25,37 +15,5 @@ class DashboardController extends Controller
     public function indexAction()
     {
         return $this->render('dashboard/index.html.twig');
-    }
-
-    /**
-     * Method displays projects list from Redmine.
-     *
-     * @Route("/dashboard/projects", name="projects")
-     */
-    public function showProjectsAction()
-    {
-        $data = $this->redmineRequestService->getProjects();
-
-        $projectForm = $this->createForm(ProjectType::class);
-        return $this->render('dashboard/projects/projects.html.twig', [
-            'project' => $projectForm->createView(),
-            'projects' => $data['projects']
-        ]);
-    }
-
-    /**
-     * Method displays projects details from Redmine.
-     *
-     * @Route("/dashboard/projects/{projectId}", requirements={"projectId" = "\d+"}, name="project_details")
-     */
-    public function showProjectDetailsAction(Request $request, $projectId)
-    {
-        $projectDetails = $this->redmineRequestService->getProjectDetails($projectId);
-        $issues = $this->redmineRequestService->getIssuesByProjectId($projectId);
-
-        return $this->render('dashboard/projects/project.html.twig', [
-            'details' => $projectDetails,
-            'issues' => $issues['issues']
-        ]);
     }
 }
