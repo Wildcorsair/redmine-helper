3573249cf3622c97598d9b0543d9c07c20de705e
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-04-02 21:03:17 +0300

    It was moved gulpfile.js and package.json files from public directory, and removed CDN

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp   = require('gulp'),
+    sass   = require('gulp-sass'),
+    concat = require('gulp-concat');
+
+// SASS to CSS task
+gulp.task('sass', function() {
+    return gulp.src('web/assets/sass/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('web/css'));
+});
+
+// Copy libraries
+gulp.task('copy-libs', function () {
+  return gulp.src('web/assets/libs/')
+      .pipe(gulp.dest('web/libs/'));
+});
+
+gulp.task('build', ['sass', 'copy-libs'], function() {
+    console.log('Modules was successfully built!');
+});
+
+// Watch task
+gulp.task('watch', function() {
+    gulp.watch('web/assets/sass/*.sass', ['sass']);
+});
